45f7946adcd79f5b0d4a5af5d89393d259849ade
胡啸天 <huxiaotian@huxiaotiandeMacBook-Air.local>
2024-05-20 21:43:29 -0700

    [gaoMoney] Refine eval_consec_days.py
    [Description]
    Refine the UI, supports analyze multiple stocks

--- eval_consec_days.py
+++ eval_consec_days.py
@@ -1,28 +1,29 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
+import argparse
 
-def get_data_before(end_date_str, span):
+def get_data_before(stock, end_date_str, span):
     end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
     start_date = end_date - timedelta(days = span)
     start_date_str = start_date.strftime("%Y-%m-%d")
 
 
     # Use the Ticker module to get the S&P 500 data
-    sp500 = yf.Ticker("^GSPC")
+    sp500 = yf.Ticker(stock)
 
     # Get historical market data
     return sp500.history(start = start_date_str, end = end_date_str)
 
-def get_diff_pct_data_before(end_date_str, span):
-    return get_data_before(end_date_str, span)['Close'].pct_change()*100
+# def get_diff_pct_data_before(end_date_str, span):
+#     return get_data_before(end_date_str, span)['Close'].pct_change()*100
     
-    return diff_series.values[1:]
+#     return diff_series.values[1:]
 
-def get_diff_data_before(end_date_str, span):
-    return get_data_before(end_date_str, span)['Close'].diff()
+# def get_diff_data_before(end_date_str, span):
+#     return get_data_before(end_date_str, span)['Close'].diff()
     
-    return diff_series.values[1:]
+#     return diff_series.values[1:]
 
 # check the possible P(A|B) = P(AB)/P(B),
 # B: for all data in the time-window, B_policy is true
@@ -81,21 +82,46 @@ class DualWindows(SlideWindow):
     def A_hold(self, data, p):
         return self.m_ap(data[p: p + self.m_ws])
 
-if __name__ == "__main__":
-    import sys
-    data = get_diff_pct_data_before(sys.argv[1], int(sys.argv[2])*365)
+def parse_arg():
+    parser = argparse.ArgumentParser(description='what would happen if consecutive drops has happened?')
+    parser.add_argument('--start-date', '-s', type=str, default=datetime.today().date().strftime("%Y-%m-%d"))
+    parser.add_argument('--span-in-yr', '-sy', type=int, default=10)
+    parser.add_argument('--consecutive-days', '-c', type=int, default=5)
+
+    parser.add_argument('--stocks', '-st', metavar='string', type=str, nargs='+', default=["^GSPC"])
+
+
+    args = parser.parse_args()
+    
+    return args
+
+def check_stock(stock, args):
+    print("check stock ", stock)
+
+    print("")
+    data = get_data_before(stock, args.start_date, args.span_in_yr*365)
     print("length of data:", data.size)
 
     print("when consective drop happens, the probability that the next day rise")
     rise = lambda x: x > 0
     drop = lambda x: x <= 0
-    SlideWindow(int(sys.argv[3]), rise, drop)(data)
+    SlideWindow(args.consecutive_days, rise, drop)((data['Close'].pct_change()*100).values[1:])
     
     print("")
-    print("")
-    print("")
     print("when consective drop happens, the probability that marcket price come back after the same days")
-    data2 = get_diff_data_before(sys.argv[1], int(sys.argv[2])*365)
+    # data2 = get_diff_data_before(args.start_date, args.span_in_yr*365*365)
     rise_back = lambda vec: vec[-1] > vec[0]
     all_drop = lambda vec: all([x <= 0 for x in vec])
-    DualWindows(int(sys.argv[3]), rise_back, all_drop)(data)
+    DualWindows(args.consecutive_days, rise_back, all_drop)(data['Close'].diff().values[1:])
+
+if __name__ == "__main__":
+    args = parse_arg()
+
+    print("what happened if market drop in {} consecutive days, the record check {} years starting from {}".format(
+        args.consecutive_days, args.span_in_yr, args.start_date
+    ))
+    
+    for s in args.stocks:
+        check_stock(s, args)
+
+
